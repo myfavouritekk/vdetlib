073ef52b08b3331b12284d889bbc79863c7e4492
Kai KANG <myfavouritekk@gmail.com>
2015-09-25 23:54:56 +0800

    Refactoring in video det.

--- vdet/video_det.py
+++ vdet/video_det.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import copy
 from dataset import imagenet_vdet_classes
-from proposal import get_windows
+from proposal import vid_proposals
 from ..utils.protocol import empty_det_from_box, score_proto, bbox_hash, det_score
 from ..utils.common import imread
 from ..utils.cython_nms import vid_nms
@@ -15,40 +15,24 @@ def det_vid_with_box(vid_proto, box_proto, det_fun, net,
     det_proto = empty_det_from_box(box_proto)
     for frame in vid_proto['frames']:
         frame_id, path = frame['frame'], frame['path']
-        img = imread(os.path.join(root, path))
         det_cur_frame = \
             [i for i in det_proto['detections'] if i['frame'] == frame_id]
-        print "Detecting in frame {}, {} boxes...".format(
-            frame_id, len(det_cur_frame))
-        for det in det_cur_frame:
-            scores = det_fun(img, det['bbox'], net)
-            det['scores'] = score_proto(class_names, scores)
+        if len(det_cur_frame) > 0:
+            print "Detecting in frame {}, {} boxes...".format(
+                frame_id, len(det_cur_frame))
+            img = imread(os.path.join(root, path))
+            for det in det_cur_frame:
+                scores = det_fun(img, det['bbox'], net)
+                det['scores'] = score_proto(class_names, scores)
     return det_proto
 
 
 def det_vid_without_box(vid_proto, det_fun, net,
                     class_names=imagenet_vdet_classes):
-    root = vid_proto['root_path']
-    det_proto = {}
-    det_proto['video'] = vid_proto['video']
-    detections = []
-    frame_names = [os.path.join(root, frame['path']) for frame in vid_proto['frames']]
     print "Generating proposals..."
-    all_boxes = get_windows(frame_names)
-    print "Detecting..."
-    for frame, boxes in zip(vid_proto['frames'], all_boxes):
-        print "Detecting in frame {}, {} boxes...".format(
-            frame['frame'], len(boxes))
-        img = imread(os.path.join(root, frame['path']))
-        for bbox in boxes:
-            det = {}
-            det['frame'] = frame['frame']
-            det['bbox'] = bbox.tolist()
-            det['hash'] = bbox_hash(det_proto['video'], frame['frame'], bbox)
-            scores = det_fun(img, bbox, net)
-            det['scores'] = score_proto(class_names, scores)
-            detections.append(det)
-    det_proto['detections'] = detections
+    box_proto = vid_proposals(vid_proto)
+    det_proto = det_vid_with_box(vid_proto, box_proto, det_fun, net,
+                                 class_names)
     return det_proto
 
 
