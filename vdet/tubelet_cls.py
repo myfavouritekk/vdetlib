cb9687c1d1340b1c9558a4334101f6ea812d46e8
Kai KANG <myfavouritekk@gmail.com>
2015-10-26 16:39:24 +0800

    Add rcnn scoring functions.

--- vdet/tubelet_cls.py
+++ vdet/tubelet_cls.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-from ..utils.protocol import frame_path_at, track_box_at_frame, bbox_hash
-from ..utils.common import imread
+from ..utils.protocol import frame_path_at, track_box_at_frame, bbox_hash, \
+    tubelets_overlap, tubelets_proto_from_tracks_proto, tubelet_box_at_frame
+from ..utils.common import imread, svm_from_rcnn_model
 from ..utils.log import logging
-from ..vdet.image_det import googlenet_det
+from ..vdet.image_det import googlenet_det, googlenet_features, svm_scores
+from ..vdet.dataset import imagenet_vdet_classes, index_vdet_to_det
 import numpy as np
 import sys
 import os
@@ -53,6 +55,46 @@ def googlenet_cls(video_proto, track_proto, net, class_idx):
                     "hash": bbox_hash(video_proto['video'], frame_id, box)
                 })
     return new_tracks
+
+
+def rcnn_scoring(vid_proto, track_proto, net, class_idx, rcnn_model):
+    svm_model = svm_from_rcnn_model(rcnn_model)
+    tubelets_proto = tubelets_proto_from_tracks_proto(track_proto['tracks'], class_idx)
+    logging.info("Scoring {} for {}...".format(vid_proto['video'],
+                 imagenet_vdet_classes[class_idx]))
+    for frame in vid_proto['frames']:
+        frame_id = frame['frame']
+        img = imread(frame_path_at(vid_proto, frame_id))
+        boxes = [tubelet_box_at_frame(tubelet, frame_id) \
+                 for tubelet in tubelets_proto]
+        valid_boxes = np.asarray([box for box in boxes if box is not None])
+        valid_index = [i for i, box in enumerate(boxes) if box is not None]
+        logging.info("frame {}: {} boxes".format(frame_id, len(valid_index)))
+        features = googlenet_features(img, valid_boxes, net, 'pool5')
+        scores = svm_scores(features, svm_model)
+        if scores.shape[1] == 200:
+            cls_scores = scores[:, index_vdet_to_det[class_idx] - 1]
+        else:
+            raise
+        for score, tubelet_id in zip(cls_scores, valid_index):
+            cur_box = [box for box in tubelets_proto[tubelet_id]['boxes'] \
+                if box['frame'] == frame_id]
+            assert len(cur_box) == 1
+            cur_box[0]['det_score'] = score
+    return tubelets_proto
+
+
+def scoring_tracks(vid_proto, track_proto, annot_proto,
+        sc_method, net, class_idx):
+    assert vid_proto['video'] == track_proto['video']
+    score_proto = {}
+    score_proto['video'] = vid_proto['video']
+    score_proto['method'] = sc_method.__name__
+    tubelets_proto = sc_method(vid_proto, track_proto, net, class_idx)
+    if annot_proto is not None:
+        tubelets_proto = tubelets_overlap(tubelets_proto, annot_proto, class_idx)
+    score_proto['tubelets'] = tubelets_proto
+    return score_proto
 
 
 def classify_tracks(video_proto, track_proto, cls_method, net, class_idx):
