cc5e5ad9f2fd25bd195d1c0c87aa69a5652bff85
Kai KANG <myfavouritekk@gmail.com>
2015-11-20 16:26:01 +0800

    Add script to generate vid protos.

---
+++ tools/gen_vid_proto_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '../..'))
+from vdetlib.utils.common import stem
+from vdetlib.utils.protocol import vid_proto_from_dir, proto_dump
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vid_name')
+    parser.add_argument('root_dir')
+    parser.add_argument('out_file')
+    args = parser.parse_args()
+
+    if os.path.isfile(args.out_file):
+        print "{} already exists.".format(args.out_file)
+        sys.exit(0)
+    vid = vid_proto_from_dir(args.root_dir, args.vid_name)
+    proto_dump(vid, args.out_file)
